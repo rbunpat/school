7e3017b1233fac92902f664b7af8f3f719446e9a
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2022-06-10 11:31:30 +0700

    Update irs.py

--- st/irs.py
+++ st/irs.py
@@ -3,7 +3,7 @@ input1 = input("Enter a number: ")
 #add 7 percent vat and 30 percent profit to input1
 saleprice = float(input1) * 1.07 + float(input1) * 0.3
 #add 30 percent profit to input1
-ogandprofit = float(input1) * 0.3
+ogandprofit = float(input1) * 0.3 + float(input1)
 
 #print out the result
 
